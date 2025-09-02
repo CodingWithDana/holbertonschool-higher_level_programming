a0ba77de6a6557370b2d104bd1e6f388936aac6b
Dana Le <danadaole@gmail.com>
2025-09-03 00:53:50 +1000

    print concatenated strings

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# This script concatenates two strings
 str1 = "Holberton"
 str2 = "School"
+# concatenate str1 and str2 and print the message
 print(f"Welcome to {str1} {str2}!")
