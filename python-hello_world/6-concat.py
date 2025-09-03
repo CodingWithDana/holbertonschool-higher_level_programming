3983eb52b0bde7045b538ea14ee3af01e9284a58
Dana Le <danadaole@gmail.com>
2025-09-03 11:53:56 +1000

    fix pycode

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-# This line uses the variable str1
-print(f"Welcome to {str1} {str2}!")
+str3 = str1 + " " + str2
+print(f"Welcome to {str3}!")
