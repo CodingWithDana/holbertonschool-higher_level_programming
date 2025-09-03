bf31ca5b8a401db70c5b481fb7de8096041494d8
Dana Le <danadaole@gmail.com>
2025-09-03 23:32:08 +1000

    prints the number of and the list of its arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,14 +5,14 @@ if __name__ == "__main__":
     # excluding the script name
     argv = sys.argv[1:]
     count = len(argv)
-    
+
     if count == 0:
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
         print(f"{count} arguments:")
-    
+
     for i in range(count):
         # i+1 to start counting at 1
         print(f"{i + 1}: {argv[i]}")
