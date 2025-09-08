6ee5941fef84b7818809ebb052e9df03e201bad2
Dana Le <danadaole@gmail.com>
2025-09-09 00:02:41 +1000

    write a python script to find all multiplies of 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    # Return a new list of booleans indicating whether each number is divisible by 2
+    # A new list of booleans indicating if each number is divisible by 2
     new_list = []
     for num in my_list:
         if num % 2 == 0:
