35f3634cf69e02f816d29036711044331fc696da
Dana Le <danadaole@gmail.com>
2025-09-08 16:34:58 +1000

    write a python script to replace an element in a list at a specific position without modifying the original list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,10 +2,10 @@
 def new_in_list(my_list, idx, element):
     # Make a copy of my_list
     new_list = my_list.copy()
-    
+
     # Check if index is valid
     if 0 <= idx < len(my_list):
-    # Replace the element at a given index
+        # Replace the element at a given index
         new_list[idx] = element
-    
+
     return new_list
