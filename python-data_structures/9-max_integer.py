9a5dead5c7a0362520468682b488e2d8085b9292
Dana Le <danadaole@gmail.com>
2025-09-08 23:41:34 +1000

    write a python script to find the largest integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    my_list.sort()
-    my_list.reverse()
-    max_value = my_list[0]
-    return max_value
+    if len(my_list) != 0:
+        my_list.sort()
+        my_list.reverse()
+        max_value = my_list[0]
+        return max_value
+    return None
