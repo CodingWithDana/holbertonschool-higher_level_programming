82f00285316178f839f4750b3586b8dad77b73a8
Dana Le <danadaole@gmail.com>
2025-09-09 13:38:38 +1000

    write a python script to return a set of different items in two sets

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    # Find the differences (in set_1 not in set_2)
+    diff_1 = set(set_1) - set(set_2)
+    # Find the differences (in set_2 not in set_1)
+    diff_2 = set(set_2) - set(set_1)
+    # Combine the differences
+    result = list(diff_1 | diff_2)
+    print(result)
