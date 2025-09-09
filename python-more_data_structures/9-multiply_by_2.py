b387a5c96a08a0dc1f956cf7b28c7d4ad783eba9
Dana Le <danadaole@gmail.com>
2025-09-09 16:49:48 +1000

    write a python script that returns a new dictionary with all values multiplied by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_a_dictionary = []
-    for value in a_dictionary.values():
-        new_a_dictionary = value * 2
+    new_a_dictionary = {}
+    for key, value in a_dictionary.items():
+        new_a_dictionary[key] = value * 2
     return new_a_dictionary
