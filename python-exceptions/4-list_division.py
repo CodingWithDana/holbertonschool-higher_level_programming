ff93167912eb453405ff6982e41be74b5286226f
Dana Le <danadaole@gmail.com>
2025-09-10 14:57:02 +1000

    Write a function thatdivides element by element 2 lists

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     # Initialise result list (to store division results)
     new_list = []
-    
+
     for i in range(list_length):
         # Divide the element at index i from both lists
         try:
