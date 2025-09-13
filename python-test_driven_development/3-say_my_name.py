9be8f661e67a15c94708a9c8be8c940a061caa5c
Dana Le <danadaole@gmail.com>
2025-09-13 20:28:29 +1000

    Write a function that prints first name and last name, and a test cases file

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module docstring provides a function to
+print My name is <first name> <last name>
+
+say_my_name function validates the input,
+then prints each element accordingly
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print the first name and last name
+
+    Args:
+        first_name (str): the first name
+        last_name (str): the last name. Defaults to ""
+
+    Returns:
+        a string with the first name and last name
+  
+    Raises:
+        TypeError: if first_name or last_name is not a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
