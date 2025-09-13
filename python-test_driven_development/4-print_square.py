58511d964f62053e9cfb5fa1eb81a7dbf2d3f56f
Dana Le <danadaole@gmail.com>
2025-09-14 00:00:58 +1000

    Write a function that prints a square of hashes, and a test cases file

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -6,16 +6,16 @@ print a square with the character '#'
 print_square function validates the input,
 then prints a square with the character '#'
 """
+
+
 def print_square(size):
     """
     Prints a square with the character '#'
 
     Args:
         size (int): The size length of the square
-
     Returns:
         A square of hashes
-
     Raises:
         TypeError: if size is not an integer
         ValueError: if size is less than 0
