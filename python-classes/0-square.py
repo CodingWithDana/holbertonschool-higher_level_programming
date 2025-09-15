2da5143025f00b988f30a723aed57a5a4ba63889
Dana Le <danadaole@gmail.com>
2025-09-15 14:24:45 +1000

    add README.md file and solution for task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Nameless modue for the Square class """
+
+
+class Square:
+    """
+    A square
+    
+    Args:
+        None (because the class Square is empty)
+        
+    Returns:
+        Nothing
+        
+    """
