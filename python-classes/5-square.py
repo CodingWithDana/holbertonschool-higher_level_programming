87620572ed1deca276cb0a4f78f1cca6863e74f5
Dana Le <danadaole@gmail.com>
2025-09-16 23:52:04 +1000

    write a class Square that defines a class and create a public instance method my_print()

---
+++ python-classes/5-square.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+""" Module for the Square class """
+
+
+class Square:
+    """
+    A class that defines a square
+
+    Attributes:
+        __size (int): size of the square (private)
+
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialise the square
+
+        Args:
+            size: the size of the square (defaults to 0)
+
+        """
+        # Use the property setter for validation
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Get the size of the square
+
+        Returns:
+            int: size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square with validation
+
+        Args:
+            value (int): new size of the square
+
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    # Add public instance method area()
+    def area(self):
+        """
+        Return the current square area
+
+        Returns:
+            int: area of the square
+        """
+        return self.__size * self.__size
+    
+    # Add public instance method my_print()
+    def my_print(self):
+        """
+        Print the square using #
+        
+        If size is 0, print an empty line
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
