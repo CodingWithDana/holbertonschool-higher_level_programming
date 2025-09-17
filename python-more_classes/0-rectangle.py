b6f0abe7b5192a213d1a9b9b5aa4dc9369eaaeef
Dana Le <danadaole@gmail.com>
2025-09-17 12:40:57 +1000

    write an empty clss Rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Nameless module for the Rectangle class """
+
+
+class Rectangle:
+    """
+    A rectangle
+
+    Args:
+        None (because the class Rectangle is empty)
+
+    Returns:
+        Nothing
+
+    """
