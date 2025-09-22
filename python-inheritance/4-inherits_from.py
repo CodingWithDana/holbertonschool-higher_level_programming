ac32f45c30772c242e4aa7ce7dc9861148789531
Dana Le <danadaole@gmail.com>
2025-09-22 17:23:02 +1000

    checks if obj is an instance of a subclass of a_class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Writes a function that if the object
+    is an instance of a class that inherited (directly or indirectly) 
+    from the specified class
+    otherwise False
+"""
+
+
+def inherits_from(obj, a_class):
+    """ Returns True if obj is an instance of a subclass of a_class """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
