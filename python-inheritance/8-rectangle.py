160dc6724f5df4bd50dbd92bbc43efe6a4ea4fdd
Dana Le <danadaole@gmail.com>
2025-09-23 00:57:24 +1000

    creates new class Rectangle that inherits from BaseGeometry class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,26 +1,7 @@
 #!/usr/bin/python3
 """ Defines BaseGeometry and Rectangle """
 
-
-class BaseGeometry:
-    """ A base class for geometry objects """
-
-    def area(self):
-        """ Public instance method to raise a custom Exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-            Public instance method to validate value
-
-            Args:
-            name (str): name of the parameter (used in error message)
-            value (int): value to validate
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ Define new class Rectangle that inherits from BaseGeometry """
@@ -31,7 +12,7 @@ class Rectangle(BaseGeometry):
         # Validate width and height using BaseGeometry method: integer_validator
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        
+    
         # Private attributes
         self.__width = width
         self.__height = height
