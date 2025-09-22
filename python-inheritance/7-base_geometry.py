7982701910ff7d929e8cc651f2456de3e53d4506
Dana Le <danadaole@gmail.com>
2025-09-23 00:32:27 +1000

    adds public instance method integer_validator()

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,7 +4,7 @@
 
 class BaseGeometry:
     """ A base class for geometry objects """
-    
+
     def area(self):
         """ Public instance method to raise a custom Exception """
         raise Exception("area() is not implemented")
