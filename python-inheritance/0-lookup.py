95f12803b15bf8068b6c305ed0e2b1ac7b015892
Dana Le <danadaole@gmail.com>
2025-09-22 15:55:27 +1000

    returns a list of all attributes and methods of an object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-""" Defines the lookup function that returns all attributes and methods of an object """
+""" 
+    Defines the lookup function that returns all attributes
+    and methods of an object 
+"""
+
+
 def lookup(obj):
     """ Return a list of all available attributes and methods of obj """
     # dir() already returns a list
