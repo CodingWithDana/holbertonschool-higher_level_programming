a9e643ee3ab4011bfc3e9ac16d3db44565904069
Dana Le <danadaole@gmail.com>
2025-09-23 10:54:04 +1000

    defines Square class that inherits the validator method from Rectangle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ Defines BaseGeometry and Rectangle """
-
 Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
     """ Square class """
-
 
     def __init__(self, size):
         """ Initialise the Square with size """
