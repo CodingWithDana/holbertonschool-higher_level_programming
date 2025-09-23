01a78ec2e9080d0f7bd2946cca910bdc2df5bfd8
Dana Le <danadaole@gmail.com>
2025-09-23 15:40:39 +1000

    create abstract class Animal and subclasses Dog, Cat

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,13 +1,17 @@
 from abc import ABC, abstractmethod
+
 # create an abstract class Animal using ABC package
 class Animal(ABC):
     @abstractmethod
     def sound(self):
         pass
 
-# create two subclasses: Dog & Cat
+# subclass Dog
 class Dog(Animal):
-    super().sound("Bark")
-    
+    def sound(self):
+        return "Bark"
+
+# subclass Cat
 class Cat(Animal):
-    super().sound("Meow")
+    def sound(self):
+        return "Meow"
