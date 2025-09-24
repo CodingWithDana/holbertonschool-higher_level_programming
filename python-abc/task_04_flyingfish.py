5f8e013b1d43952b970fea07486deea785c31e33
Dana Le <danadaole@gmail.com>
2025-09-24 16:08:53 +1000

    FlyingFish class: multiple inheritance

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -1,7 +1,7 @@
 class Fish:
     def swim(self):
         print("The fish is swimming")
-    
+
     def habitat(self):
         print("The fish lives in water")
 
@@ -9,7 +9,7 @@ class Fish:
 class Bird:
     def fly(self):
         print("The bird is flying")
-    
+
     def habitat(self):
         print("The bird lives in the sky")
 
@@ -17,19 +17,19 @@ class Bird:
 class FlyingFish(Fish, Bird):
     def swim(self):
         print("The flying fish is swimming!")
-        
+ 
     def fly(self):
         print("The flying fish is soaring!")
-        
+
     def habitat(self):
         print("The flying fish lives both in water and the sky!")
-        
+
 # instantiate
 flying_fish = FlyingFish()
 # call methods: fly, swim, habitat
-swim(flying_fish)
-fly(flying_fish)
-habitat(flying_fish)
+flying_fish.swim()
+flying_fish.fly()
+flying_fish.habitat()
 
 # use mro() method
 print(FlyingFish.mro())
