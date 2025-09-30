d1411e87946fd51c10cdb87212dd93fbbc8a3fdc
Dana Le <danadaole@gmail.com>
2025-09-30 23:42:44 +1000

    basic serialization and deserialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
     Module for adding the functionality to serialize a Python dictionary to a JSON file and
     deserialize the JSON file to recreate the Python Dictionary
 """
@@ -15,21 +15,22 @@ def serialize_and_save_to_file(data, filename):
             filename: The filename of the output JSON file.
                     If the output file already exists,
                     it should be replaced
-        
+
         Return: None (because this function just writes to a file)
     """
-    with open("data.json", "w") as filename:
-        json.dump(data, filename, indent=4)
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
 
 
 def load_and_deserialize(filename):
     """
         Deserialise JSON from file back into a dictionary
-        
+
         Args:
             filename: filename of the input JSON file
 
-        Return: Python Dictionary with the deseialised JSON data from the file.
+        Return:
+            dict: The deserialised data from the JSON file.
     """
-    with open("data.json", "r") as filename:
-        data_loaded = json.load(filename)
+    with open(filename, "r") as f:
+        return json.load(f)
