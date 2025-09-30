dcdbebdb89b1803354b1f1df1bad786357ab10c6
Dana Le <danadaole@gmail.com>
2025-09-30 11:34:09 +1000

    returning the JSON representation of an object (string)

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-    Module for returning the JSON representation of 
+    Module for returning the JSON representation of
     an object (string)
 """
-
 import json
+
+
 def to_json_string(my_obj):
     """
     Returning the JSON representation of an object (string)
