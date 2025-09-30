7bc61a06194267ceffc7edea0d916fc14a23caab
Dana Le <danadaole@gmail.com>
2025-09-30 12:45:59 +1000

    return a dictionary representation of an object's attributes that can be serialised to json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Module for returning the dictionary description 
+    with simple data structures (list, dictionary, string,
+    integer, and boolean) of a class instance
+"""
+
+def class_to_json(obj):
+    """
+        Return the dictionary description with simple data structures (list, 
+        dictionary, string,integer, and boolean) of a class instance
+        
+        Args:
+            dict: Dictionary of the object's attributes
+    """
+    return obj.__dict__
