8d460f2064d6fed3c431c44d88fdc5c03477bb6e
Dana Le <danadaole@gmail.com>
2025-09-30 11:01:11 +1000

    write a string to text file and return the length of characters added

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module for writing a string to a text file and returning the length of characters written """
+
+
+def write_file(filename="", text=""):
+    """
+    Write a string to a text file and return the length of characters written
+
+    Args:
+        filename (str):the path to the file to read(defaults to empty string)
+
+    Return:
+        none
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        characters_written = f.write(text)
+
+    print(characters_written)
