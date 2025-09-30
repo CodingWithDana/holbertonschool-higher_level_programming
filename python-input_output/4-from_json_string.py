7af57b484b043a3bfd9ef28ee20ee63e8137d0bf
Dana Le <danadaole@gmail.com>
2025-09-30 11:38:55 +1000

    returning an object (string) from json formatted string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Module for returning an object 
+    (Python data structure) represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returning the JSON representation of an object (string)
+    """
+    return json.loads(my_obj)
