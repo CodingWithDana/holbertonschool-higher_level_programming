ac844e69ad8f8bb4a6d0250b02d44c6fed75980f
Dana Le <danadaole@gmail.com>
2025-09-30 12:55:04 +1000

    define Student class including the public method to retrieve a dictionary representation of the class's instance

---
+++ python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Student:
+    """ 
+        Defines a Student class including their:
+        first name, last name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        """ Instantiate the public instance attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Retrieve a dictionary representation of the Student instance """
+        return self.__dict__
