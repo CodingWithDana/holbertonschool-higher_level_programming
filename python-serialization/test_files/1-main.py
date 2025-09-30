4fffbecaf9c7647248dc0c46505007dfdc6b2c0f
Dana Le <danadaole@gmail.com>
2025-10-01 00:51:14 +1000

    serialization and deserialization using pickle module

---
+++ python-serialization/test_files/1-main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from task_01_pickle import CustomObject
+
+# Create an instance of CustomObject
+obj = CustomObject(name="John", age=25, is_student=True)
+print("Original Object:")
+obj.display()
+
+# Serialize the object
+obj.serialize("object.pkl")
+
+# Deserialize the object into a new instance
+new_obj = CustomObject.deserialize("object.pkl")
+print("\nDeserialized Object:")
+new_obj.display()
+
+# Output:
+
+# Original Object:
+# Name: John
+# Age: 25
+# Is Student: True
+
+# Deserialized Object:
+# Name: John
+# Age: 25
+# Is Student: True
