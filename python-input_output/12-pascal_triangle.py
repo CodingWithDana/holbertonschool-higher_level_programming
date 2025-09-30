a4d90fd54f9a6b82ca6599e87af76406abe3eb27
Dana Le <danadaole@gmail.com>
2025-09-30 18:00:56 +1000

    return a list of lists of integers representing the Pascal's triangle of n

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+    Module for returning a list of lists of integers
+    representing the Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+        Return a list of lists representing Pascal's Triangle of n
+
+        Args: 
+            n (int): number of rows
+
+        Return:
+            list: list of lists of integers 
+    """
+
+    # Check: empty list
+    if n <= 0:
+        return []
+
+    # First row
+    triangle = [[1]]
+
+    for i in range (1, n):
+        prev_row = triangle[-1]
+        # Each row starts and ends with 1
+        row = [1]
+        # Compute middle values
+        for j in range(1, len(prev_row)):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
