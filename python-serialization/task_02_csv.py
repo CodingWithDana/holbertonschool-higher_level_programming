4d0210ce60c07e08467ccb7a35f9837d4f055612
Dana Le <danadaole@gmail.com>
2025-10-01 13:44:07 +1000

    convert a CSV file to JSON

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,29 @@
+import csv
+import json
+
+
+def convert_csv_to_json(csv_filename: str) -> bool:
+    """
+    Convert a CSV file to JSON and save it as data.json
+
+    Args:
+        csv_filename (str): the path to the CSV file
+
+    Return:
+        bool: True if successful, otherwise False
+    """
+    try:
+        # read CSV file
+        with open(csv_filename, mode ='r', newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            # convert rows to list of dicts
+            data = [ row for row in reader]
+
+        # write JSON data to file
+        with open('data.json', 'w', encoding='utf-8') as jsonfile:
+            json.dump(data, jsonfile, indent=4)
+
+        return True
+
+    except Exception:
+        return False
