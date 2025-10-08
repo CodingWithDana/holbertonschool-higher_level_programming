7338b9068705d4426806d2217662bdeacfcb9d1a
Dana Le <danadaole@gmail.com>
2025-10-08 23:41:48 +1100

    set up simple API using Flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+
+
+# instantiate a Flask web server from the Flask module
+app = Flask(__name__)
+
+# in memory data storage
+users = {}
+
+@app.route("/")
+def home():
+    # root route
+    return "Welcome to the Flask API!"
+
+@app.route("/status")
+def status():
+    # status endpoint
+    return "OK"
+
+@app.route("/data")
+def get_users():
+    # return a list of all usernames
+    return jsonify(list(users.key()))
+
+@app.route("/users/<username>")
+def get_user(username):
+    # return full user data related to a provided username
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    return jsonify({"error": "User not found"}), 404
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    # add a new user through POST 
+    data = request.get_json()
+    if not data or "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
+    
+    username = data["username"]
+    users[username] = data
+    return jsonify({"message": "User added", "user": data}), 201
