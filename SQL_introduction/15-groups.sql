b34904bd9a7cf91f15197baf1cd4d6b291b2f76b
Dana Le <danadaole@gmail.com>
2025-10-14 12:40:15 +1100

    group records with same score and sort desc

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 -- list number of records with the same score in the second_table
-SELECT score COUNT(*) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
 ORDER BY number DESC;
