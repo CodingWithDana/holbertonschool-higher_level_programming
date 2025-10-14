7bb179ce1eb022ec137a93eaa554f1feb186282b
Dana Le <danadaole@gmail.com>
2025-10-14 12:52:52 +1100

    list all records where name column has a value and sort the record descending

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+-- list all records with certain conditions
+    -- do not list rows where name column has no value
+    -- score and name are outputed in this order
+        -- score   name
+        -- 18  Aria
+        -- 12  Aria
+        -- 10  John
+        -- 10  Bob
+    -- records are sorted by descending score
+    -- database name will be passed in the command line (no action required in the code)
+
+SELECT (*) FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
