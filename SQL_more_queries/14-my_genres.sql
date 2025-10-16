4c4c9ec7667d9ee2dfd51e78411b6e07dc16d0f5
Dana Le <danadaole@gmail.com>
2025-10-16 22:59:48 +1100

    list all genres of a specific show

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -5,9 +5,9 @@
     -- each record should display: tv_genres.name
     -- results must be sorted in ascending order by the genre name
     -- can use only one SELECT statement
-SELECT DISTINCT tv_genres.name AS name
+SELECT tv_genres.name AS name
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY name ASC;
