9283538a2209a159e1088638a073d257117f081d
Dana Le <danadaole@gmail.com>
2025-10-16 13:49:28 +1100

    create a table and add data

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+-- create the table force_name
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
+
+INSERT INTO force_name (id, name)
+VALUES (59, "DANA");
