6df8b9695b8009b50fc8833c6834e86116b24e07
Dana Le <danadaole@gmail.com>
2025-10-16 16:52:59 +1100

    subquery with using JOIN

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,6 @@
 
 -- list all cities in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE cities.state_id = states.state_id
+FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
