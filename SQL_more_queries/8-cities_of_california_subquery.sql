f5a7313aadfb918ef65fd1069fc7099229ca91f4
Dana Le <danadaole@gmail.com>
2025-10-16 16:28:50 +1100

    create a subquery

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 
--- 
+-- look up specfied data
+USE hbtn_0d_usa;
+
+SELECT *
+FROM CITIES
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY id ASC;
