a0def94d1a09c2b6d0731ffa3cda6abf400b7ae9
Dana Le <danadaole@gmail.com>
2025-10-16 12:36:12 +1100

    create a new user and grant all priviledges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 
 -- create the server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'host' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
