5a171d09dc2e42caa35a73a2ac7fb732d71c1541
Dana Le <danadaole@gmail.com>
2025-10-16 22:18:47 +1100

    list all genres that have shows

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+    -- each record should display: <TV Show genre> - <Number of shows linked to this genre>
+    -- first column must be called genre
+    -- second column must be called number_of_shows
+    -- don’t display a genre that doesn’t have any shows linked
+    -- results must be sorted in descending order by the number of shows linked
+    -- you can use only one SELECT statement
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
