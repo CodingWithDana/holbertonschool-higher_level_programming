89c763e1f2ad9a2eaec17a431d2e9a54e4513eb1
Dana Le <danadaole@gmail.com>
2025-10-16 21:24:34 +1100

    list all shows that has no genre linked

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+--  list all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+WHERE tv_shows.id != tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
