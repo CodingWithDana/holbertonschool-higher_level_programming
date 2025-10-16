fccc76985892fbde23530724e83f8315c52a2d23
Dana Le <danadaole@gmail.com>
2025-10-16 16:42:48 +1100

    subquery - only use SELECT statement once

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 -- list all cities in the database hbtn_0d_usa
-SELECT id, cities.name, states.name
-FROM hbtn_0d_usa
+SELECT 
+    cities.id,
+    cities.name,
+    (SELECT states.name
+    FROM states
+    WHERE states.id = cities.state_id
+    )
+FROM cities
 ORDER BY cities.id ASC;
