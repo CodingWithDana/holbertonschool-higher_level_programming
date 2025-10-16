86c6e11fc096ca4913c0d35b9a3aa84824b917d4
Dana Le <danadaole@gmail.com>
2025-10-16 22:34:29 +1100

    list all genres of a specific show

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -5,8 +5,7 @@
     -- each record should display: tv_genres.name
     -- results must be sorted in ascending order by the genre name
     -- can use only one SELECT statement
-SELECT tv_genres.name
+SELECT DISTINCT tv_genres.name AS name
 FROM tv_genres
-LEFT JOIN tv_shows ON tv_shows.id = tv_genres.id
-WHERE tv_shows.id = 8
-ORDER BY tv_genres.name ASC;
+INNER JOIN tv_show_genres ON tv_show_genres.genre.id = tv_genres.id
+ORDER BY name ASC;
