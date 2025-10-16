2ca6ad30df9cf5cb132593fbdbd78a56112f0b0c
Dana Le <danadaole@gmail.com>
2025-10-16 20:46:57 +1100

    list shows that have at least one genre linked

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 
 -- list all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre.id
-FROM tv.shows, tv_show_genres
+FROM tv_shows, tv_show_genres
 WHERE tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
