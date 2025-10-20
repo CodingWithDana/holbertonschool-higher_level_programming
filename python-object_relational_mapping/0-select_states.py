958f4a61aae9df7ab2f9fd03e047ccb14a623e27
Dana Le <danadaole@gmail.com>
2025-10-20 23:16:28 +1100

    connect Python to MySQL using MySQLdb module

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,21 +8,22 @@ import MySQLdb
 import sys
 # write a script that lists all states from the database hbtn_0e_0_usa:
 # your script should take 3 arguments: 
-#         mysql username, mysql password and database name (no argument validation needed)
+#         mysql username, mysql password and database name
 # use the module MySQLdb (import MySQLdb)
-# your script should connect to a MySQL server running on localhost at port 3306
+# connect to a MySQL server running at port 3306
 # results must be sorted in ascending order by states.id
 # your code should not be executed when imported
 
 
 # your code should not be executed when imported
-if __name__== "__main__":
+if __name__ == "__main__":
     # Get command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connect to MySQL server using provided credentials to interact with the database
+    # Connect to MySQL server using provided credentials
+    # to interact with the database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -33,7 +34,7 @@ if __name__== "__main__":
 
     # Create a cursor object to send the SQL query to MySQL
     cur = db.cursor()
-    
+
     # Execute SQL query
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
