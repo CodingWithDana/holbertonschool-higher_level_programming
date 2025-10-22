c63c8b802b0bd4029d622fdfd5b6cb92f7c5a775
Dana Le <danadaole@gmail.com>
2025-10-22 23:27:26 +1100

    prints all cities of a chosen state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Take the name of a state as an argument and lists all cities 
+of that state, using the database hbtn_0e_4_usa
+"""
+# take 4 arguments: `mysql username`,`mysql password`, `database name`,
+# `state name`
+# use the module `MySQLdb` (import MySQLdb)
+# script should connect to a MySQL server running on localhost at port 3306
+# results must be sorted in ascending order by cities.id
+# can use only execute() once
+# your code should not be executed when imported
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # get command-line args
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # create a cursor to execute queries
+    cursor = db.cursor()
+
+    # execute SQL query to  lists all cities of that state
+    cursor.execute(
+        "SELECT cities_name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states_name = %s "
+        "ORDER BY cities.id ASC;"
+    )
+
+    # fetch and display all matching rows
+    for row in cursor.fetchall():
+        print(row)
+
+    # close the connection
+    cursor.close()
+    db.close()
