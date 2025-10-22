febde16b8b994aa5a2ccb3938b7a23db59055b17
Dana Le <danadaole@gmail.com>
2025-10-22 23:20:18 +1100

    select all

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -32,10 +32,10 @@ if __name__ == "__main__":
 
     # execute SQL query to display all cities
     cursor.execute(
-        "SELECT cities.id, cities.name, states.name"
-        "FROM cities"
-        "JOIN states ON cities.state_id = states.id"
-        "ORDER BY cities.id ASC"
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC;"
     )
 
     # fetch and display all matching rows
