fe3431e953c449b6bd82b5b52d4e1662afc5ecd1
Dana Le <danadaole@gmail.com>
2025-10-22 23:38:15 +1100

    prints all cities of a chosen state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Take the name of a state as an argument and lists all cities 
+Take the name of a state as an argument and lists all cities
 of that state, using the database hbtn_0e_4_usa
 """
 # take 4 arguments: `mysql username`,`mysql password`, `database name`,
