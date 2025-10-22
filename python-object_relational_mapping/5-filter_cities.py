8f3a073716c0e22d9f85f945c1baeb7dff53631d
Dana Le <danadaole@gmail.com>
2025-10-22 23:29:37 +1100

    prints all cities of a chosen state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -39,8 +39,7 @@ if __name__ == "__main__":
         "SELECT cities_name "
         "FROM cities "
         "JOIN states ON cities.state_id = states.id "
-        "WHERE states_name = %s "
-        "ORDER BY cities.id ASC;"
+        "WHERE states_name = %s ORDER BY cities.id ASC;"
     )
 
     # fetch and display all matching rows
