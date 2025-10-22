c4d3bca5a29392263f2c1f1da58d6cfb5bc7bc53
Dana Le <danadaole@gmail.com>
2025-10-23 00:08:01 +1100

    create Session object

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,7 +4,7 @@ List all State objects from the database hbtn_0e_6_usa
 """
 # take 3 arguments: `mysql username`,`mysql password`, `database name`,
 # use the module `SQLAlchemy`
-# must import State and Base from model_state 
+# must import State and Base from model_state
 # - `from model_state import Base, State`
 # script should connect to a MySQL server running on localhost at port 3306
 # results must be sorted in ascending order by states.id
