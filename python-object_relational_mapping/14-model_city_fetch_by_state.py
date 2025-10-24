0bf09ca9d95dcc033e6034cdf8b6aad0fb6820b4
Dana Le <danadaole@gmail.com>
2025-10-24 15:32:30 +1100

    list all City objects from database using SQLAlchemy

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -14,6 +14,7 @@ Print all City objects from the database hbtn_0e_14_usa
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 from model_city import Base, City
 
 
@@ -38,8 +39,8 @@ if __name__ == "__main__":
     session = Session()
 
     # query all City objects, sorted by cities.id in ASC order
-    for city in session.query(City).order_by(City.id):
-        print(f"{city.id}: {city.name}")
+    for city in session.query(City).join(State).order_by(City.id).all():
+        print(f"{city.state.name}: ({city.id}) {city.name}")
 
     # Close the session
     session.close()
