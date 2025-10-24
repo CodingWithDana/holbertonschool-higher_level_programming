f0d7e02d853a536f9977e40cbc6f4c2c79dd9b58
Dana Le <danadaole@gmail.com>
2025-10-24 15:35:10 +1100

    list all City objects from database using SQLAlchemy

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,7 +15,7 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from model_city import Base, City
+from model_city import City
 
 
 if __name__ == "__main__":
