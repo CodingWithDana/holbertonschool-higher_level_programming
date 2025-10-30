5ab69297e185d0a376ad073f9c31b56723e40616
Dana Le <danadaole@gmail.com>
2025-10-30 23:00:29 +1100

    print the first argument

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const arg = process.argv[2];
-if (arg == '') {
+if (arg === undefined || arg === null || arg === '' || arg === 0 || arg === false) {
   console.log('No argument');
 } else {
   console.log(arg);
